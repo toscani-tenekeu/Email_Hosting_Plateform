d91c0d4d5231ea17411c726e45be2150a2da3bb4
TENEKEU MODJOU TOSCANI <hello@toscani-tenekeu.com>
2026-08-04 06:41:40 +0100

    Add application configuration

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const siteUrl = import.meta.env.VITE_SITE_URL || 'https://email-hosting.kmerhosting.com'
+export const domainStoreUrl = import.meta.env.VITE_DOMAIN_STORE_URL || 'https://domain.kmerhosting.com'
+export const supportEmail = import.meta.env.VITE_SUPPORT_EMAIL || 'support@kmerhosting.com'
